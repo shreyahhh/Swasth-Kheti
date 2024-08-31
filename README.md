1bcde39eb8a53eefc7dd5857e527a2fc0092db9f
lokesh0802 <lokesh08519137@gmail.com>
2024-08-31 10:53:51 +0530

    poush

--- README.md
+++
@@ -1,1 +0,0 @@
-# Swasth-Kheti
--- backend/plant/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('predict/', views.predict, name='predict'),
-    path('chat/', views.chat, name='chat'),
-]
